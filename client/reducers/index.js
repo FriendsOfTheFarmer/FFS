d9e17476fb5a2de7b35963c28a655cdc9c7ba7b8
Luke Van Bergen <lukepvb@gmail.com>
2020-01-04 14:33:57 -0800

    initial customer_component commit

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,11 +3,14 @@ import { combineReducers } from 'redux';
 import aReducer from './aReducer';
 import vendorReducer from './vendorReducer';
 import renderingReducer from './renderingReducer';
+import customerReducer from './customerReducer';
 
 const reducers = combineReducers({
   generic: aReducer,
   vendor: vendorReducer,
-  rendering: renderingReducer
+  rendering: renderingReducer,
+  customer: customerReducer
+
 })
 
 
