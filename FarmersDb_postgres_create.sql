60c8a0aa3e6e87d80ee9a3db28838f0b78bfd4b8
danst3in <50937807+danst3in@users.noreply.github.com>
2020-01-04 11:06:27 -0800

    corrected schema and updated the schema script, add png and recreat db

--- FarmersDb_postgres_create.sql
+++ FarmersDb_postgres_create.sql
@@ -1,85 +1,98 @@
-CREATE TABLE "Market" (
-	"Market_Id" serial NOT NULL,
-	"Market_Name" varchar(255) NOT NULL UNIQUE,
-	"Market_Location" varchar(255) NOT NULL,
-	"Market_Details" varchar(500) NOT NULL,
-	"Market_Hours_Id" serial NOT NULL,
-	"Market_Vendor_Id" serial NOT NULL UNIQUE,
-	CONSTRAINT "Market_pk" PRIMARY KEY ("Market_Id")
+CREATE TABLE "market" (
+	"market_id" serial NOT NULL,
+	"market_name" varchar(255) NOT NULL UNIQUE,
+	"market_location" varchar(255) NOT NULL,
+	"market_details" varchar(500) NOT NULL,
+	CONSTRAINT "market_pk" PRIMARY KEY ("market_id")
 ) WITH (
   OIDS=FALSE
 );
 
 
 
-CREATE TABLE "Business_Hours" (
-	"Hours_Id" serial NOT NULL,
-	"Hours_Day" integer NOT NULL,
-	"Hours_Open" TIME NOT NULL,
-	"Hours_Close" TIME NOT NULL,
-	CONSTRAINT "Business_Hours_pk" PRIMARY KEY ("Hours_Id")
+CREATE TABLE "business_hours" (
+	"hours_id" serial NOT NULL,
+	"hours_day" integer NOT NULL,
+	"hours_open" TIME NOT NULL,
+	"hours_close" TIME NOT NULL,
+	"market_hours_id" integer NOT NULL,
+	CONSTRAINT "business_hours_pk" PRIMARY KEY ("hours_id")
 ) WITH (
   OIDS=FALSE
 );
 
 
 
-CREATE TABLE "Vendor" (
-	"Vendor_Id" serial NOT NULL,
-	"Vendor_Name" varchar(255) NOT NULL UNIQUE,
-	"Vendor_Phone" integer NOT NULL,
-	"Vendor_Website" varchar(255) NOT NULL,
-	"Vendor_Email" varchar(255) NOT NULL,
-	"Vendor_Bio" varchar(500) NOT NULL,
-	"Vendor_Market_Id" serial NOT NULL UNIQUE,
-	"Vendor_Hours_Id" integer NOT NULL,
-	"Vendor_Item_Id" serial NOT NULL UNIQUE,
-	CONSTRAINT "Vendor_pk" PRIMARY KEY ("Vendor_Id")
+CREATE TABLE "vendor" (
+	"vendor_id" serial NOT NULL,
+	"vendor_name" varchar(255) NOT NULL UNIQUE,
+	"vendor_phone" integer NOT NULL,
+	"vendor_website" varchar(255) NOT NULL,
+	"vendor_email" varchar(255) NOT NULL,
+	"vendor_bio" varchar(500) NOT NULL,
+	CONSTRAINT "vendor_pk" PRIMARY KEY ("vendor_id")
 ) WITH (
   OIDS=FALSE
 );
 
 
 
-CREATE TABLE "Market_Vendor" (
-	"Market_Vendor_Id" serial NOT NULL,
-	"Market_Vendor_Date" DATE NOT NULL,
-	CONSTRAINT "Market_Vendor_pk" PRIMARY KEY ("Market_Vendor_Id")
+CREATE TABLE "date" (
+	"date_id" serial NOT NULL,
+	"market_vendor_date" DATE NOT NULL,
+	"market_id" integer NOT NULL,
+	"vendor_id" integer NOT NULL,
+	CONSTRAINT "date_pk" PRIMARY KEY ("date_id")
 ) WITH (
   OIDS=FALSE
 );
 
 
 
-CREATE TABLE "Item" (
-	"Item_Id" serial NOT NULL,
-	"Item_Name" varchar(255) NOT NULL UNIQUE,
-	"Vendor_Item_Id" serial(255) NOT NULL UNIQUE,
-	CONSTRAINT "Item_pk" PRIMARY KEY ("Item_Id")
+CREATE TABLE "item" (
+	"item_id" serial NOT NULL,
+	"item_name" varchar(255) NOT NULL UNIQUE,
+	CONSTRAINT "item_pk" PRIMARY KEY ("item_id")
 ) WITH (
   OIDS=FALSE
 );
 
 
 
-CREATE TABLE "Vendor_Item" (
-	"Vendor_Item_Id" serial NOT NULL,
-	"Vendor_Item_Details" varchar(255) NOT NULL,
-	"Vendors_Items_Price" numeric(4,2) NOT NULL,
-	CONSTRAINT "Vendor_Item_pk" PRIMARY KEY ("Vendor_Item_Id")
+CREATE TABLE "vendor_item" (
+	"vendor_item_id" serial NOT NULL,
+	"vendors_items_price" numeric(4,2) NOT NULL,
+	"vendor_item_details" varchar(255) NOT NULL,
+	"vendor_id" integer NOT NULL,
+	"item_id" integer NOT NULL,
+	CONSTRAINT "vendor_item_pk" PRIMARY KEY ("vendor_item_id")
 ) WITH (
   OIDS=FALSE
 );
 
 
 
-ALTER TABLE "Market" ADD CONSTRAINT "Market_fk0" FOREIGN KEY ("Market_Hours_Id") REFERENCES "Business_Hours"("Hours_Id");
-ALTER TABLE "Market" ADD CONSTRAINT "Market_fk1" FOREIGN KEY ("Market_Vendor_Id") REFERENCES "Market_Vendor"("Market_Vendor_Id");
+CREATE TABLE "date_item" (
+	"date_item_id" serial NOT NULL,
+	"vendor_item_id" integer NOT NULL,
+	"date_id" integer NOT NULL,
+	CONSTRAINT "date_item_pk" PRIMARY KEY ("date_item_id")
+) WITH (
+  OIDS=FALSE
+);
 
 
-ALTER TABLE "Vendor" ADD CONSTRAINT "Vendor_fk0" FOREIGN KEY ("Vendor_Market_Id") REFERENCES "Market_Vendor"("Market_Vendor_Id");
-ALTER TABLE "Vendor" ADD CONSTRAINT "Vendor_fk1" FOREIGN KEY ("Vendor_Hours_Id") REFERENCES "Business_Hours"("Hours_Id");
-ALTER TABLE "Vendor" ADD CONSTRAINT "Vendor_fk2" FOREIGN KEY ("Vendor_Item_Id") REFERENCES "Vendor_Item"("Vendor_Item_Id");
 
 
-ALTER TABLE "Item" ADD CONSTRAINT "Item_fk0" FOREIGN KEY ("Vendor_Item_Id") REFERENCES "Vendor_Item"("Vendor_Item_Id");
+ALTER TABLE "business_hours" ADD CONSTRAINT "business_hours_fk0" FOREIGN KEY ("market_hours_id") REFERENCES "market"("market_id");
+
+
+ALTER TABLE "date" ADD CONSTRAINT "date_fk0" FOREIGN KEY ("market_id") REFERENCES "market"("market_id");
+ALTER TABLE "date" ADD CONSTRAINT "date_fk1" FOREIGN KEY ("vendor_id") REFERENCES "vendor"("vendor_id");
+
+
+ALTER TABLE "vendor_item" ADD CONSTRAINT "vendor_item_fk0" FOREIGN KEY ("vendor_id") REFERENCES "vendor"("vendor_id");
+ALTER TABLE "vendor_item" ADD CONSTRAINT "vendor_item_fk1" FOREIGN KEY ("item_id") REFERENCES "item"("item_id");
+
+ALTER TABLE "date_item" ADD CONSTRAINT "date_item_fk0" FOREIGN KEY ("vendor_item_id") REFERENCES "vendor_item"("vendor_item_id");
+ALTER TABLE "date_item" ADD CONSTRAINT "date_item_fk1" FOREIGN KEY ("date_id") REFERENCES "date"("date_id");
